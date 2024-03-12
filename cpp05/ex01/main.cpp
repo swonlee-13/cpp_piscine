7aa5d073e6539ad690886decde09266ef811ec41
seongwol <gbssw13@gmail.com>
2024-03-13 02:13:35 +0900

    Style: test line

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -8,17 +8,17 @@ int main()
 		Bureaucrat seongwol("seongwol", 150);
 		Form Plan("War machine plan", 100, 100);
 
-		std::cout << "-------------signForm test(Wrong)----------" << std::endl;
+		std::cout << "-------------signForm test(Wrong)----------------" << std::endl;
 		seongwol.signForm(Plan);
 		std::cout << std::endl;
-		std::cout << "-------------Form << test before sign.----------" << std::endl;
+		std::cout << "-------------Form << test before sign.-----------" << std::endl;
 		std::cout << Plan << std::endl << std::endl;
 
-		std::cout << "-------------signForm test(normal)----------" << std::endl;
+		std::cout << "-------------signForm test(normal)---------------" << std::endl;
 		bob.signForm(Plan);
 		std::cout << std::endl;
 
-		std::cout << "-------------Form << test after sign.----------" << std::endl;
+		std::cout << "-------------Form << test after sign.------------" << std::endl;
 		std::cout << Plan << std::endl << std::endl;
 	}
 	catch (std::exception &e) {
