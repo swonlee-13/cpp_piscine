83439e2f334d93ce8f42170ac9e977d57fd7a1ff
seongwol <gbssw13@gmail.com>
2024-03-14 15:07:45 +0900

    feat: Finished cpp05/ex02

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -1,19 +1,32 @@
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+
 int main()
 {
 	try {
 		Bureaucrat bob("bob", 1);
 		// Bureaucrat hi("hi", 151);
 		Bureaucrat seongwol("seongwol", 3);
-		AForm *Plant = new RobotomyRequestForm("Tree");
-
+		AForm *Plant = new ShrubberyCreationForm("Tree");
+		AForm *Robot = new RobotomyRequestForm("Robot");
+		AForm *Coffee_man = new PresidentialPardonForm("Coffee-man");
+		
 		std::cout << "-------------Execute test----------" << std::endl;
 		seongwol.signForm(*Plant);
+		seongwol.signForm(*Robot);
+		seongwol.signForm(*Coffee_man);
+		std::cout << std::endl;
 		seongwol.executeForm(*Plant);
 		std::cout << std::endl;
+		seongwol.executeForm(*Coffee_man);
+		std::cout << std::endl;
+		seongwol.executeForm(*Robot);
+		std::cout << std::endl;
 		delete Plant;
+		delete Robot;
+		delete Coffee_man;
 	}
 	catch (char const * e) {
 		std::cerr << e << std::endl;
