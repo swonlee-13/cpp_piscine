30a5f448dc3a8fb3e9371a4d0bd88d693b625f54
seongwol <gbssw13@gmail.com>
2024-03-21 18:36:26 +0900

    Fix: remove unused header

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -3,11 +3,26 @@
 
 int main()
 {
-	Array<int> A(2);
-	A[0] = 1;
-	A[1] = 2;
-	for (int i = 0; i < 2; i++) {
-		std::cout << A[i] << std::endl;
+	{
+		std::cout << "----------normal test----------" << std::endl;
+		Array<int> A(2);
+		A[0] = 1;
+		A[1] = 2;
+		for (int i = 0; i < 2; i++) {
+			std::cout << A[i] << std::endl;
+		}
+		std::cout << "size: " << A.size() << std::endl;
 	}
-	std::cout << "size: " << A.size() << std::endl;
+	{
+		Array<int> A(2);
+		std::cout << "----------test -1 index with catch------------" << std::endl;
+		try {
+			std::cout << A[-1] << std::endl;
+		}
+		catch (std::exception &e) {
+			std::cerr << e.what() << std::endl;
+		}
+		std::cout << "----------test -1 index without catch (Abort occurs)----------" << std::endl;
+		std::cout << A[3] << std::endl;
+	}
 }
