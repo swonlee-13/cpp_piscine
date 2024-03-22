715fa95a63953729cb09994846344dd1cee326cf
Seongwon Lee <seongwol@c3r8s3.42seoul.kr>
2024-03-22 13:33:56 +0900

    Fix: fix second parameter as template F

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -3,9 +3,8 @@
 # include <cstddef>
 # include <iostream>
 
-template <typename T>
-
-void iter(T *array, std::size_t length, void (*f)(T &)) {
+template <typename T, typename F>
+void iter(T *array, std::size_t length, F f) {
 	for (std::size_t i = 0; i < length; i++)
 		f(array[i]);
 }
