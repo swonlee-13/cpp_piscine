f507da8df2471bba1936156c87d0147354fc7403
Seongwon Lee <seongwol@c3r8s3.42seoul.kr>
2024-06-22 16:29:47 +0900

    remove -g flag at ex01/makefile && add ac < 2 statement at ex02/main.cpp

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "PmergeMe.hpp"
 
-
-
 int main(int ac, char **av)
 {
+	if (ac < 2) {
+		std::cerr << "invalid argment." << std::endl;
+		return EXIT_FAILURE;
+	}
 	PmergeMe A(ac, av);
 	if (A.validCheck(ac, av) == false) {
 		std::cerr << "Error" << std::endl;
